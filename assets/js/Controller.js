78339291d6c398b8da53d93d7244f7a936166a04
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2021-11-30 22:32:03 +0100

    implementing trash

--- assets/js/Controller.js
+++ assets/js/Controller.js
@@ -18,7 +18,7 @@ export default class Controller{
   initialize = () => {
     this.evaluateButton.addEventListener('click', this.evaluateHandler) 
     this.dotButton.addEventListener('click', this.dotHandler) 
-    this.popup.addEventListener('click', this.removePopup) 
+    this.popup.addEventListener('click', this.popupHandler) 
     this.backspaceKey.addEventListener('click', this.backHandler) 
     this.operatorButtons.forEach( 
       operator => operator.addEventListener('click', this.operatorHandler) 
@@ -29,7 +29,6 @@ export default class Controller{
     document.querySelectorAll('.memory input').forEach( 
       number => number.addEventListener('click', this.memoryHandler)    
     )
-    document.querySelector('.popup .content').addEventListener('click', this.popupHandler) 
     document.getElementById('history').addEventListener('click', this.historyHandler) 
     document.getElementById('clear').addEventListener('click', this.clearHandler) 
     document.getElementById('clear-entry').addEventListener('click', this.clearEntryHandler) 
@@ -150,9 +149,13 @@ export default class Controller{
       }
   }
   popupHandler = (event) => {
-    if(event.target.tagName === 'svg' || event.target.tagName === 'path') {
+    if(event.target.classList.contains('popup'))
+      event.target.classList.remove('show')
+    else if(event.target.tagName === 'svg') {
       this[event.target.dataset.from].clear()
       this.popup.querySelector('.content').innerHTML = ''
+      this.popup.querySelector('.trash').innerHTML = ''
+      return
     }
     if(event.target.tagName === 'INPUT') {
       this.memoryHandler({
@@ -163,11 +166,6 @@ export default class Controller{
       })
       this.fillPopup('memory')
     }
-    
-    //   return
-    // else
-      
-    // this.fillPopup('memory')
   }
   historyHandler = () => {
     this.popup.classList.add('show')
@@ -194,15 +192,12 @@ export default class Controller{
       this.display.displayResult(historyState.result)
     }
   }
-  removePopup = (event) => {
-    if(event.target.classList.contains('content')) return
-    event.target.classList.remove('show')
-  } 
 
   fillPopup = (from) => {
-    let html
+    let contentHtml = '', trashHtml = ''
+
     if(from === 'memory')
-      html = this.memory.getAll().map( (el, i) => 
+      contentHtml = this.memory.getAll().map( (el, i) => 
         `<div class="memory-item hover2" >
           <h1>${el}</h1>
           <div class="memory-control" data-index="${i}">
@@ -213,15 +208,17 @@ export default class Controller{
         </div>`  
       ).reverse().join(' ')
     else if(from === 'history')
-      html = this.history.getAll().map( (el, i) => 
+      contentHtml = this.history.getAll().map( (el, i) => 
       `<div class="history-item hover2" data-index="${i}" >
         <p>${el.operand1}  ${el.operator}  ${el.operand2} = </p>
         <h2>${el.result}</h2>
       </div>`  
       ).reverse().join(' ')      
 
-    this.popup.querySelector('.content').innerHTML = html
-    this.popup.querySelector('.trash').innerHTML = `<svg data-from="${from}" aria-hidden="true" focusable="false" data-prefix="fa-light" data-icon="trash-can" class="svg-inline--fa fa-trash-can fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M432 64h-96l-33.63-44.75C293.4 7.125 279.1 0 264 0h-80C168.9 0 154.6 7.125 145.6 19.25L112 64h-96C7.201 64 0 71.2 0 80c0 8.799 7.201 16 16 16h416c8.801 0 16-7.201 16-16C448 71.2 440.8 64 432 64zM152 64l19.25-25.62C174.3 34.38 179 32 184 32h80c5 0 9.75 2.375 12.75 6.375L296 64H152zM400 128C391.2 128 384 135.2 384 144v288c0 26.47-21.53 48-48 48h-224C85.53 480 64 458.5 64 432v-288C64 135.2 56.84 128 48 128S32 135.2 32 144v288C32 476.1 67.89 512 112 512h224c44.11 0 80-35.89 80-80v-288C416 135.2 408.8 128 400 128zM144 416V192c0-8.844-7.156-16-16-16S112 183.2 112 192v224c0 8.844 7.156 16 16 16S144 424.8 144 416zM240 416V192c0-8.844-7.156-16-16-16S208 183.2 208 192v224c0 8.844 7.156 16 16 16S240 424.8 240 416zM336 416V192c0-8.844-7.156-16-16-16S304 183.2 304 192v224c0 8.844 7.156 16 16 16S336 424.8 336 416z" fill="currentColor"/></svg>`
+    if(contentHtml) trashHtml = `<svg data-from="${from}" aria-hidden="true" focusable="false" data-prefix="fa-light" data-icon="trash-can" class="svg-inline--fa fa-trash-can fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M432 64h-96l-33.63-44.75C293.4 7.125 279.1 0 264 0h-80C168.9 0 154.6 7.125 145.6 19.25L112 64h-96C7.201 64 0 71.2 0 80c0 8.799 7.201 16 16 16h416c8.801 0 16-7.201 16-16C448 71.2 440.8 64 432 64zM152 64l19.25-25.62C174.3 34.38 179 32 184 32h80c5 0 9.75 2.375 12.75 6.375L296 64H152zM400 128C391.2 128 384 135.2 384 144v288c0 26.47-21.53 48-48 48h-224C85.53 480 64 458.5 64 432v-288C64 135.2 56.84 128 48 128S32 135.2 32 144v288C32 476.1 67.89 512 112 512h224c44.11 0 80-35.89 80-80v-288C416 135.2 408.8 128 400 128zM144 416V192c0-8.844-7.156-16-16-16S112 183.2 112 192v224c0 8.844 7.156 16 16 16S144 424.8 144 416zM240 416V192c0-8.844-7.156-16-16-16S208 183.2 208 192v224c0 8.844 7.156 16 16 16S240 424.8 240 416zM336 416V192c0-8.844-7.156-16-16-16S304 183.2 304 192v224c0 8.844 7.156 16 16 16S336 424.8 336 416z" fill="currentColor"/></svg>`
+    
+    this.popup.querySelector('.content').innerHTML = contentHtml
+    this.popup.querySelector('.trash').innerHTML = trashHtml
   }
 
   /**
